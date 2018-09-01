e2ad00a6a2968762053361b3adb57ce62db7333d
Teoman Shipahi <aytekoz@yahoo.com>
2018-08-31 22:45:57 -0400

    regular expressions

---
+++ apps/app-book/src/org/gamegogo/RegularExpressionsChapter/DemonstratingPatternMatching2/RegExpr2.java
@@ -0,0 +1,15 @@
+package org.gamegogo.RegularExpressionsChapter.DemonstratingPatternMatching2;
+
+// Use find() to find a subsequence.
+
+import java.util.regex.*;
+
+class RegExpr2 {
+    public static void main(String args[]) {
+        Pattern pat = Pattern.compile("Java");
+        Matcher mat = pat.matcher("Java 8");
+        System.out.println("Looking for Java in Java 8.");
+        if (mat.find()) System.out.println("subsequence found");
+        else System.out.println("No Match");
+    }
+}
