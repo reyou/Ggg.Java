3d972229bb20dd6b66292a2c66be878b52cf7b4f
teoman shipahi <aytekoz@yahoo.com>
2019-07-05 16:41:21 -0400

    files changed

--- apps/apps-book/JavaEE8Recipes/Chapter1-WorkingWithServlets/intro7-Listening-for-Servlet-Container-Events/intro1-web/src/main/java/com/reyou/ggg/intro1/web/StartupShutdownListener.java
+++ apps/apps-book/JavaEE8Recipes/Chapter1-WorkingWithServlets/intro7-Listening-for-Servlet-Container-Events/intro1-web/src/main/java/com/reyou/ggg/intro1/web/StartupShutdownListener.java
@@ -7,6 +7,17 @@ package com.reyou.ggg.intro1.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,14 +32,14 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "StartupShutdownListener", urlPatterns = {"/StartupShutdownListener"})
 public class StartupShutdownListener extends HttpServlet {
 
-    public void contextInitialized(ServletContextEvent event) {
+    public void contextInitialized(ServletContextEvent event) throws IOException {
         System.out.println("Servlet startup...");
         System.out.println(event.getServletContext().getServerInfo());
         System.out.println(System.currentTimeMillis());
         sendEmail("Servlet context has initialized");
     }
 
-    public void contextDestroyed(ServletContextEvent event) {
+    public void contextDestroyed(ServletContextEvent event) throws IOException {
         System.out.println("Servlet shutdown...");
         System.out.println(event.getServletContext().getServerInfo());
         System.out.println(System.currentTimeMillis());
@@ -47,6 +58,8 @@ public class StartupShutdownListener extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        System.out.println("request.getContextPath:");
+        System.out.println(request.getContextPath());
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
@@ -101,8 +114,56 @@ public class StartupShutdownListener extends HttpServlet {
         return "Short description";
     }// </editor-fold>
 
-    private void sendEmail(String message) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private boolean sendEmail(String message) throws IOException {
+        boolean result = false;
+        String smtpHost = "smtp.gmail.com";
+        String smtpUsername = GetTempFileContent("email.txt");
+        String smtpPassword = GetTempFileContent("password.txt");
+        String from = GetTempFileContent("fromaddress.txt");
+        String to = GetTempFileContent("toaddress.txt");
+        int smtpPort = 587;
+        System.out.println("sending email...");
+        try {
+// Send email here
+//Set the host smtp address
+            Properties props = new Properties();
+            props.put("mail.smtp.host", smtpHost);
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+            // create some properties and get the default Session
+            Session session = Session.getInstance(props);
+
+            // create a message
+            Message msg = new MimeMessage(session);
+
+            // set the from and to address
+            InternetAddress addressFrom = new InternetAddress(from);
+            msg.setFrom(addressFrom);
+            InternetAddress[] address = new InternetAddress[1];
+            address[0] = new InternetAddress(to);
+            msg.setRecipients(Message.RecipientType.TO, address);
+            msg.setSubject("Servlet container shutting down");
+            // Append Footer
+            msg.setContent(message, "text/plain");
+            Transport transport = session.getTransport("smtp");
+            transport.connect(smtpHost, smtpPort, smtpUsername, smtpPassword);
+            Transport.send(msg);
+
+        } catch (MessagingException ex) {
+            ex.printStackTrace();
+            result = false;
+        }
+        return result;
+    }
+
+    private String GetTempFileContent(String emailtxt) throws IOException {
+        Path path = Paths.get("C:\\temp\\" + emailtxt);
+        String text = "";
+        List<String> readAllLines = Files.readAllLines(path);
+        for (String line : readAllLines) {
+            text += line;
+        }
+        return text;
     }
 
 }
